ebe866acbacae8c0f213823604967b6a81bd2f81
Pedro Luis Marconatto <160744952+Panelin123@users.noreply.github.com>
2025-05-05 11:31:59 -0300

    main.c

--- ProjetosC-main/calculadora/main.c
+++ ProjetosC-main/calculadora/main.c
@@ -6,33 +6,33 @@ int main()
     float num1, num2, resultado;
     char operador;
 
-    printf("Digite o Operador(+, -, * /, %) \n");
-    scanf("%c", &operador);
+    printf("Digite o Operador (+, -, *, /):\n");
+    scanf(" %c", &operador);
 
-    printf("Digite o primeiro numero: \n");
-    scanf("%d", &num1);
+    printf("Digite o primeiro numero:\n");
+    scanf("%f", &num1);
 
-    printf("Digite o segundo numero: \n");
-    scanf("%d", &num2);
+    printf("Digite o segundo numero:\n");
+    scanf("%f", &num2);
 
-    if(operador == '+'){
-        resultado = num1 +num2;
-        printf("O resultado da soma é: %d \n", resultado);
-    }
-
-    if(operador == '-')
-    resultado = num1 - num2;
-    printf("O resultado da soma é: %d \n", resultado);
-    }
-
-    if(operador == '*')
-    resultado = num1 * num2;
-    printf("O resultado da soma é: %d \n", resultado);
-    }
-
-    if(operador == '/')
-    resultado = num1 / num2;
-    printf("O resultado da soma é: %d \n", resultado);
+    if (operador == '+') {
+        resultado = num1 + num2;
+        printf("O resultado da soma Ã©: %.2f\n", resultado);
+    } else if (operador == '-') {
+        resultado = num1 - num2;
+        printf("O resultado da subtracao Ã©: %.2f\n", resultado);
+    } else if (operador == '*') {
+        resultado = num1 * num2;
+        printf("O resultado da multiplicacao Ã©: %.2f\n", resultado);
+    } else if (operador == '/') {
+        if (num2 == 0) {
+            printf("Erro: divisao por zero!\n");
+        } else {
+            resultado = num1 / num2;
+            printf("O resultado da divisao Ã©: %.2f\n", resultado);
+        }
+    } else {
+        printf("Operador invalido.\n");
     }
 
     return 0;
